9af722f57c7d217c4c3ca4af71dbf807dae7e4d3
Matt Juffs <matt@slickhouse.com>
2017-09-25 15:55:02 +0100

    Visual Studio solution created

---
+++ SlickCMS/Extension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlickCMS
+{
+    public static class Extension
+    {
+        // TODO: move Slickhouse.Extension to here
+    }
+}
