a47e44be8d074e1f864f0f0e647980a92f240ef5
SCARIF\Matt Juffs <matthew.juffs@gmail.com>
2019-02-13 16:01:12 +0000

    Reinstated some removed code

--- SlickCMS.Web/Startup.cs
+++ SlickCMS.Web/Startup.cs
@@ -24,11 +24,11 @@ using Microsoft.Extensions.DependencyInjection;*/
 
 //using WebMarkupMin.AspNetCore2;
 
-/*using SlickCMS.Data;
+using SlickCMS.Data;
 using Microsoft.EntityFrameworkCore;
 using SlickCMS.Data.Services;
 using SlickCMS.Data.Entities;
-using SlickCMS.Data.Interfaces;*/
+using SlickCMS.Data.Interfaces;
 
 namespace SlickCMS.Web
 {
@@ -37,18 +37,18 @@ namespace SlickCMS.Web
         public IConfiguration Configuration { get; }
         public IHostingEnvironment HostingEnvironment { get; }
 
-        //public Startup(IConfiguration configuration, IHostingEnvironment env)
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         //public Startup()
-        public Startup(IConfiguration configuration)
+        //public Startup(IConfiguration configuration)
         {
             this.Configuration = configuration;
-            //this.HostingEnvironment = env;
+            this.HostingEnvironment = env;
         }
 
         // TODO: https://docs.microsoft.com/en-us/aspnet/core/performance/response-compression?view=aspnetcore-2.2
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        /*public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
@@ -57,16 +57,16 @@ namespace SlickCMS.Web
                 //options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            services.AddMvc();//.SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             //var connectionString = new SlickCMS.Core.ConnectionString();
             //services.AddDbContext<SlickCMSContext>(options => options.UseSqlServer(connectionString.Get(this.HostingEnvironment.ContentRootPath)));// mjtest
 
             // add Entity Services
-            //services.AddScoped<IPostService, PostService>();
-        }*/
+            services.AddScoped<IPostService, PostService>();
+        }
 
-        public void ConfigureServices(IServiceCollection services)
+        /*public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
@@ -77,11 +77,11 @@ namespace SlickCMS.Web
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }
+        }*/
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         // see https://docs.microsoft.com/en-us/aspnet/core/fundamentals/middleware/?view=aspnetcore-2.2#order
-        /*public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -115,9 +115,9 @@ namespace SlickCMS.Web
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
-        }*/
+        }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        /*public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -139,6 +139,6 @@ namespace SlickCMS.Web
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
-        }
+        }*/
     }
 }
